d622b1807f4d5a5dd8f80bb3657aa8f9da9617fc
Noam Yorav-Raphael <noamraph@gmail.com>
2013-10-26 22:59:26 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tqdm',
+      description='A Simple Python Progress Meter',
+      author='Noam Yorav-Raphael',
+      author_email='noamraph@gmail.com',
+      url='https://github.com/noamraph/tqdm',
+      py_modules=['tqdm'],
+     )
