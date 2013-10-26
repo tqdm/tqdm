2ef7f7c6757e430f7a8a8302416ada65a6170b96
Noam Yorav-Raphael <noamraph@gmail.com>
2013-10-26 23:06:04 +0300

    Update setup.py with version (1.0)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from distutils.core import setup
 
 setup(name='tqdm',
+      version='1.0',
       description='A Simple Python Progress Meter',
       author='Noam Yorav-Raphael',
       author_email='noamraph@gmail.com',
