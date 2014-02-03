2f46d5468b7eaabfb23081669e6c1c2760a1bc16
Ivan Ivanov <ivan@ivanovs.info>
2014-02-03 22:59:28 +1300

    Test of format_meter (failed on py32)

--- tests.py
+++ tests.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
-from tqdm import format_interval
+from tqdm import format_interval, format_meter
 
 
 def test_format_interval():
     assert format_interval(60) == '01:00'
     assert format_interval(6160) == '1:42:40'
     assert format_interval(238113) == '66:08:33'
+
+
+def test_format_meter():
+    assert format_meter(231, 1000, 392) == \
+        "|##--------| 231/1000  23% [elapsed: " \
+        "06:32 left: 12:49,  0.00 iters/sec]"
 
