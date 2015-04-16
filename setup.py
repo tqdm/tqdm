4c979245b5d7749b381b22daa11ed719369eed9d
Mikhail Korobov <kmike84@gmail.com>
2015-04-17 04:01:18 +0500

    add license and Python version info to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from setuptools import setup
-setup(name='tqdm',
-      version='1.0',
-      description='A Simple Python Progress Meter',
-      author='Noam Yorav-Raphael',
-      author_email='noamraph@gmail.com',
-      url='https://github.com/noamraph/tqdm',
-      py_modules=['tqdm'],
-     )
+
+setup(
+    name='tqdm',
+    version='1.0',
+    description='A Simple Python Progress Meter',
+    license='MIT License',
+    author='Noam Yorav-Raphael',
+    author_email='noamraph@gmail.com',
+    url='https://github.com/noamraph/tqdm',
+    py_modules=['tqdm'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+    ],
+)
