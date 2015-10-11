971f96dfa7ee0f09c7753226f16eb7a0ff4cd843
Hadrien Mary <hadrien.mary@gmail.com>
2015-10-11 19:01:12 +0200

    Add small test for version variable

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -2,7 +2,7 @@ from ._tqdm import tqdm
 from ._tqdm import trange
 from ._tqdm import format_interval
 from ._tqdm import format_meter
-
-__all__ = ['tqdm', 'trange', 'format_interval', 'format_meter']
-
 from ._version import __version__
+
+__all__ = ['tqdm', 'trange', 'format_interval', 'format_meter', '__version__']
+
