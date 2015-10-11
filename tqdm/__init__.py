56a4910cb2119de45c813ba5ab05b508e171ae97
Stephen L <lrq3000@gmail.com>
2015-10-11 20:40:08 +0200

    Ignore version import with flake8
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -2,6 +2,6 @@ from ._tqdm import tqdm
 from ._tqdm import trange
 from ._tqdm import format_interval
 from ._tqdm import format_meter
-from ._version import __version__
+from ._version import __version__ # NOQA
 
 __all__ = ['tqdm', 'trange', 'format_interval', 'format_meter', '__version__']
