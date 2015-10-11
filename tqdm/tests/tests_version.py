16e7e3e0c5606724d14b0b287c6caee4e77678c8
Hadrien Mary <hadrien.mary@gmail.com>
2015-10-11 19:01:12 +0200

    Add small test for version variable

--- tqdm/tests/tests_version.py
+++ tqdm/tests/tests_version.py
@@ -1,13 +1,3 @@
-from __future__ import unicode_literals
-
-import csv
-
-try:
-    from StringIO import StringIO
-except:
-    from io import StringIO
-
-
 def test_version():
     from tqdm import __version__
     assert len(__version__.split('.')) == 4
