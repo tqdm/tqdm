4157660a0b2d759fbc67dca93a8b8f8ddf7ede5d
Stephen L <lrq3000@gmail.com>
2015-12-02 22:21:13 +0100

    implement tqdm_notebook_pretty()
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -7,7 +7,10 @@ from ._tqdm_gui import tgrange
 from ._tqdm_pandas import tqdm_pandas
 from ._tqdm_notebook import tqdm_notebook
 from ._tqdm_notebook import tnrange
+from ._tqdm_notebook import tqdm_notebook_pretty
+from ._tqdm_notebook import tnprange
 from ._version import __version__  # NOQA
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_notebook', 'tnrange',
+           'tqdm_notebook_pretty', 'tnprange',
            'format_interval', 'format_meter', 'tqdm_pandas', '__version__']
