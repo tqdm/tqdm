5eeb0f52fc73841a0a520b6876e72fbff7477d6d
Casper da Costa-Luis <casper.dcl@physics.org>
2016-01-25 00:06:52 +0000

    fix smoothing test

--- tqdm/tests/tests_tqdm.py
+++ tqdm/tests/tests_tqdm.py
@@ -704,7 +704,7 @@ def test_smoothing():
         c2 = progressbar_rate(get_bar(our_file2, 3))
 
     # Check that medium smoothing's rate is between no and max smoothing rates
-    assert a < c < b
+    assert a == c < b
     assert a2 < c2 < b2
 
 
