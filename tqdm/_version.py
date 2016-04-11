9cdc7f65bb717b32a757f6cd959f26aae1cdaa18
Casper da Costa-Luis <casper.dcl@physics.org>
2016-04-11 20:31:31 +0100

    setup.py fix

--- tqdm/_version.py
+++ tqdm/_version.py
@@ -1,16 +1,11 @@
 # Definition of the version number
-from ._utils import _sh
+try:
+    from ._utils import _sh
+except:
+    _sh = None
+
 from subprocess import STDOUT
 __all__ = ["__version__"]
-
-
-def commit_hash(*args):
-    try:
-        return _sh('git', 'log', '-n', '1', '--oneline', *args,
-                   stderr=STDOUT).lstrip().split()[0]
-    except:
-        return None
-
 
 # major, minor, patch, -extra
 version_info = 4, 4, 0
@@ -18,9 +13,19 @@ version_info = 4, 4, 0
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
 
-cur_hash = commit_hash()
-if cur_hash is not None:
-    last_release = commit_hash('v' + __version__).rstrip(':').lower()
 
-    if 'fatal' in last_release or cur_hash not in last_release:
-        __version__ += '-' + cur_hash
+# auto -extra based on commit hash (if not tagged as release)
+if (_sh is not None) and (len(version_info) < 4):
+    def commit_hash(*args):
+        try:
+            return _sh('git', 'log', '-n', '1', '--oneline', *args,
+                       stderr=STDOUT).lstrip().split()[0]
+        except:
+            return None
+
+    cur_hash = commit_hash()
+    if cur_hash is not None:
+        last_release = commit_hash('v' + __version__).rstrip(':').lower()
+
+        if ('fatal' in last_release) or (cur_hash not in last_release):
+            __version__ += '-' + cur_hash
