088f3519319d4327373d21bd4030d5a965138e74
Casper da Costa-Luis <casper.dcl@physics.org>
2016-04-19 22:17:53 +0100

    version extra bugfix

--- tqdm/_version.py
+++ tqdm/_version.py
@@ -18,14 +18,15 @@ __version__ = '.'.join(map(str, version_info))
 if (_sh is not None) and (len(version_info) < 4):  # pragma: no cover
     def commit_hash(*args):
         try:
-            return _sh('git', 'log', '-n', '1', '--oneline', *args,
-                       stderr=STDOUT).lstrip().split()[0]
+            res = _sh('git', 'log', '-n', '1', '--oneline', *args,
+                      stderr=STDOUT).lstrip().split()[0]
+            return None if res.startswith('fatal') else res
         except:
             return None
 
     cur_hash = commit_hash()
     if cur_hash is not None:
-        last_release = commit_hash('v' + __version__).rstrip(':').lower()
+        last_release = commit_hash('v' + __version__)
 
-        if ('fatal' in last_release) or (cur_hash not in last_release):
+        if (last_release is None) or (cur_hash not in last_release):
             __version__ += '-' + cur_hash
