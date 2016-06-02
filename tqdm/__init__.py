c49021da401ac5b68892df9b71d92dab0e85167a
Yaroslav Halchenko <debian@onerussian.com>
2016-06-02 10:48:14 -0400

    RF: delay imports of ipython until necessary through adapter functions
    Partially addresses #176

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -3,8 +3,6 @@ from ._tqdm import trange
 from ._tqdm_gui import tqdm_gui
 from ._tqdm_gui import tgrange
 from ._tqdm_pandas import tqdm_pandas
-from ._tqdm_notebook import tqdm_notebook
-from ._tqdm_notebook import tnrange
 from ._main import main
 from ._main import TqdmKeyError
 from ._main import TqdmTypeError
@@ -13,3 +11,21 @@ from ._version import __version__  # NOQA
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
            'tqdm_notebook', 'tnrange', 'main', 'TqdmKeyError', 'TqdmTypeError',
            '__version__']
+
+
+def tqdm_notebook(*args, **kwargs):
+    """See tqdm._tqdm_notebook.tqdm_notebook for full documentation
+
+    This is just an adapter to delay imports
+    """
+    from ._tqdm_notebook import tqdm_notebook as _tqdm_notebook
+    return _tqdm_notebook(*args, **kwargs)
+
+
+def tnrange(*args, **kwargs):
+    """See tqdm._tqdm_notebook.tnrange for full documentation
+
+    This is just an adapter to delay imports
+    """
+    from ._tqdm_notebook import tnrange as _tnrange
+    return _tnrange(*args, **kwargs)
