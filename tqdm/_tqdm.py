1fd2234a0ba051d2502e336470fb31b6740bb68e
Casper da Costa-Luis <casper.dcl@physics.org>
2016-07-18 21:36:15 +0100

    cleaner solution

--- tqdm/_tqdm.py
+++ tqdm/_tqdm.py
@@ -308,15 +308,14 @@ class tqdm(object):
 
         # Clear all bars
         inst_cleared = []
-        if hasattr(cls, '_instances'):
-            for inst in cls._instances:
-                # Clear instance if in the target output file
-                # or if write output + tqdm output are both either
-                # sys.stdout or sys.stderr (because both are mixed in terminal)
-                if inst.fp == fp or all(f in (sys.stdout, sys.stderr)
-                                        for f in (fp, inst.fp)):
-                    inst.clear()
-                    inst_cleared.append(inst)
+        for inst in getattr(cls, '_instances', []):
+            # Clear instance if in the target output file
+            # or if write output + tqdm output are both either
+            # sys.stdout or sys.stderr (because both are mixed in terminal)
+            if inst.fp == fp or all(f in (sys.stdout, sys.stderr)
+                                    for f in (fp, inst.fp)):
+                inst.clear()
+                inst_cleared.append(inst)
         # Write the message
         fp.write(s)
         fp.write(end)
