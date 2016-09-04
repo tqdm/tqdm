3e224a5dc2b73c8b61596e59d675dccc4c9351a5
Stephen L <lrq3000@gmail.com>
2016-09-04 03:27:28 +0200

    Add lots of functionalities to tqdm_bare
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/_tqdm_bare.py
+++ tqdm/_tqdm_bare.py
@@ -10,7 +10,10 @@ Usage:
 # future division is important to divide integers and get as
 # a result precise floating numbers (instead of truncated int)
 from __future__ import division
-# import compatibility functions and utilities
+# import colorama to support move up character in windows term
+# can't be included here because conflicts if both tqdm and tqdm_bare imported
+from ._utils import IS_WIN, colorama
+# import utilities
 import sys
 from time import time
 
@@ -19,29 +22,36 @@ try:
 except:
     _range = range
 
+try:
+    _unicode = unicode
+except NameError:
+    _unicode = str
+
+# Console move up control character (opposite of newline return)
+_term_move_up = '' if IS_WIN and (colorama is None) else '\x1b[A'
+
 
 __author__ = {"github.com/": ["noamraph", "obiwanus", "kmike", "hadim",
                               "casperdcl", "lrq3000"]}
 __all__ = ['tqdm_bare', 'tbrange']
 
 
-def tqdm_bare(iterable=None, total=None, file=sys.stdout, desc='',
-                    leave=False, miniters=1, mininterval=0.1, unit='it', width=78):
+def tqdm_bare(iterable=None, desc=None, total=None, leave=True,
+                    file=sys.stdout, width=78, mininterval=0.1,
+                    maxinterval=10, miniters=None, disable=False,
+                    unit='it', unit_scale=False,
+                    smoothing=0.3, initial=0, position=None,
+                    **kwargs):
     """
     Extremely fast barebone progress bar reproducing tqdm's major features.
     Decorate an iterable object, returning an iterator which acts exactly
     like the original iterable, but prints a dynamically updating
     progressbar every time a value is requested.
+
+    See core tqdm for arguments descriptions.
+
     STATUS: ALPHA EXPERIMENTAL.
     """
-    n = [0]  # use a closure
-    start_t = [time()]
-    last_n = [0]
-    last_t = [0]
-    last_len = [0]
-    if iterable is not None:
-        total = len(iterable)
-
     def format_interval(t):
         mins, s = divmod(int(t), 60)
         h, m = divmod(mins, 60)
@@ -50,47 +60,121 @@ def tqdm_bare(iterable=None, total=None, file=sys.stdout, desc='',
         else:
             return '{0:02d}:{1:02d}'.format(m, s)
 
+    def format_sizeof(num, suffix=''):
+        """
+        Formats a number (greater than unity) with SI Order of Magnitude prefixes.
+        Parameters
+        ----------
+        num  : float
+            Number ( >= 1) to format.
+        suffix  : str, optional
+            Post-postfix [default: ''].
+        Returns
+        -------
+        out  : str
+            Number with Order of Magnitude SI unit postfix.
+        """
+        for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+            if abs(num) < 999.95:
+                if abs(num) < 99.95:
+                    if abs(num) < 9.995:
+                        return '{0:1.2f}'.format(num) + unit + suffix
+                    return '{0:2.1f}'.format(num) + unit + suffix
+                return '{0:3.0f}'.format(num) + unit + suffix
+            num /= 1000.0
+        return '{0:3.1f}Y'.format(num) + suffix
+
+    def format_meter(n, total, elapsed, ncols=None, prefix='',
+                     unit='it', unit_scale=False, rate=None):
+        """
+        Return a string-based progress bar given some parameters
+        """
+        elapsed_fmt = format_interval(elapsed)
+        if unit_scale:
+            n_fmt = format_sizeof(n)
+            total_fmt = format_sizeof(total) if total else None
+        else:
+            n_fmt = str(n)
+            total_fmt = str(total) if total else None
+
+        # if unspecified, attempt to use rate = average speed
+        # (we allow manual override since predicting time is an arcane art)
+        if rate is None and elapsed:
+            rate = n / elapsed
+        inv_rate = 1 / rate if (rate and (rate < 1)) else None
+        rate_fmt = ((format_sizeof(inv_rate if inv_rate else rate)
+                    if unit_scale else
+                    '{0:5.2f}'.format(inv_rate if inv_rate else rate))
+                    if rate else '?') \
+            + ('s' if inv_rate else unit) + '/' + (unit if inv_rate else 's')
+
+        if total:
+            frac = n / total
+            percentage = int(frac * 100)
+            eta = (total - n) / rate if rate > 0 else 0
+            eta_fmt = format_interval(eta)
+
+            l_bar = '{0}{1}%|'.format(prefix, percentage)
+            r_bar = '|{0}/{1} [{2}<{3}, {4}]'.format(n_fmt,
+                          total_fmt, elapsed_fmt, eta_fmt, rate_fmt)
+            bar_width = width - len(l_bar) - len(r_bar)
+
+            bar = '#' * int(frac * bar_width)
+            bar_length, frac_bar_length = divmod(
+                int(frac * bar_width * 10), 10)
+            bar = '#' * bar_length
+            frac_bar = chr(48 + frac_bar_length) if frac_bar_length \
+                else ''
+            bar_fill = ' ' * (bar_width - (bar_length + len(frac_bar)))
+
+            # Build complete bar
+            full_bar = l_bar + bar + frac_bar + bar_fill + r_bar
+
+        else:
+            full_bar = '{0}{1}{2} [{3}, {4}]'.format(prefix, n, unit, elapsed_fmt, rate_fmt)
+
+        return full_bar
+
+    def moveto(pos):
+        file.write(_unicode('\n' * pos + _term_move_up * -pos))
+
     def update_and_print(i=1):
+        """
+        Main function to update bar progress
+        """
+        if disable:
+            return
+
         n[0] += i
-        if (n[0] - last_n[0]) >= miniters:
+        last_iteration = (n[0] == total) if total else False
+        if (n[0] - last_n[0]) >= miniters[0] or last_iteration:
             last_n[0] = n[0]
 
-            if (time() - last_t[0]) >= mininterval:
-                last_t[0] = time()  # last_t[0] == current time
+            if (time() - last_t[0]) >= mininterval or last_iteration:
+                cur_t = time()
+                delta_t = cur_t - last_t[0]
+                delta_it = n[0] - last_print_n[0]
+                last_t[0] = cur_t
+                last_print_n[0] = n[0]
 
-                spent = last_t[0] - start_t[0]
-                spent_fmt = format_interval(spent)
-                rate = n[0] / spent if spent > 0 else 0
-                if 0.0 < rate < 1.0:
-                    rate_fmt = "%.2fs/%s" % (1.0 / rate, unit)
-                else:
-                    rate_fmt = "%.2f%s/s" % (rate, unit)
+                if dynamic_miniters:
+                    if maxinterval and mininterval and delta_t >= maxinterval:
+                        # Set miniters to correspond to maxinterval
+                        miniters[0] = delta_it * mininterval / delta_t
+                    else:
+                        # EMA-weight miniters to converge
+                        # towards the timeframe of mininterval
+                        temporalterm = (mininterval / delta_t
+                                            if mininterval and delta_t else 1)
+                        miniters[0] = smoothing * delta_it * temporalterm + \
+                                   (1 - smoothing) * miniters[0]
 
-                if total:
-                    frac = n[0] / total
-                    percentage = int(frac * 100)
-                    #eta = spent / n[0] * total if n[0] else 0
-                    eta = (total - n[0]) / rate if rate > 0 else 0
-                    eta_fmt = format_interval(eta)
+                # Format bar
+                elapsed = last_t[0] - start_t[0]
+                full_bar = format_meter(n[0], total, elapsed, width, desc, unit, unit_scale)
 
-                    l_bar = "%s: %i%%|" % (desc, percentage)
-                    r_bar = "|%i/%i [%s<%s, %s]" % (n[0],
-                                  total, spent_fmt, eta_fmt, rate_fmt)
-                    bar_width = width - len(l_bar) - len(r_bar)
-
-                    bar = "#" * int(frac * bar_width)
-                    barfill = " " * int((1.0 - frac) * bar_width)
-                    bar_length, frac_bar_length = divmod(
-                        int(frac * bar_width * 10), 10)
-                    bar = '#' * bar_length
-                    frac_bar = chr(48 + frac_bar_length) if frac_bar_length \
-                        else ' '
-
-                    # Build complete bar
-                    full_bar = l_bar + bar + frac_bar + barfill + r_bar
-
-                else:
-                    full_bar = "{0}: {1}{2} [{3}, {4}]".format(desc, n[0], unit, spent_fmt, rate_fmt)
+                # Position the cursor
+                moveto(my_pos[0])
 
                 # Clear up previous bar display
                 file.write("\r" + (" " * last_len[0]))
@@ -102,26 +186,80 @@ def tqdm_bare(iterable=None, total=None, file=sys.stdout, desc='',
                 last_len[0] = len(full_bar)
 
                 # Leave last bar display?
-                if n[0] == total:
+                if last_iteration:
                     if leave:
-                        file.write("\n")
+                        if not my_pos or my_pos[0] == 0:
+                            file.write("\n")
                     else:
                         file.write("\r" + (" " * last_len[0]) + "\r")
+                    # Remove oneself from the list of positions
+                    tqdm_bare._instances.remove(my_pos[0])
 
+                # Position the cursor back
+                moveto(-my_pos[0])
+
+                # Force output the display
                 file.flush()
 
     def update_and_yield():
+        """
+        Iterable wrapper for update_and_print()
+        """
         for elt in iterable:
             yield elt
             update_and_print()
 
+    # -- Initialization
+    n = [initial]  # use a closure to store and access within subfunctions
+    start_t = [time()]
+    last_n = [0]
+    last_print_n = [0]
+    last_t = [0]
+    last_len = [0]
+    desc = desc + ': ' if desc else ''
+
+    if miniters is None:
+        miniters = [0]
+        dynamic_miniters = True
+    else:
+        miniters = [miniters]
+        dynamic_miniters = False
+
+    # Position management
+    if not hasattr(tqdm_bare, '_instances'):
+        # Create function variable if not existent
+        tqdm_bare._instances = set()
+    if position is None:
+        # Automatic position number generation
+        prev_pos = -1  # position just before the hole
+        for p in tqdm_bare._instances:
+            # If there's a hole between two instances pos, pick this position
+            if (p - prev_pos) > 1:
+                break
+            prev_pos = p
+        # Pick next position to the one before the hole
+        position = prev_pos+1
+
+    # Assign position to current instance
+    my_pos = [position]
+    # Store position in the list (just to know which ones are taken)
+    tqdm_bare._instances.add(position)
+
+    if iterable is not None:
+        total = len(iterable)
+
     # Initialize the bar display
-    update_and_print(0)
+    moveto(my_pos[0])
+    file.write(format_meter(n[0], total, 0, width, desc, unit, unit_scale))
+    moveto(-my_pos[0])
+    # -- End of initialization
 
     # Iterable and manual mode are supported
     if iterable is not None:
+        # Iterable mode
         return update_and_yield()
     else:
+        # Manual mode
         return update_and_print
 
 
