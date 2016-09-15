8bbcab815713b35783bdf4f6d4596b15bd3b3297
Stephen L <lrq3000@gmail.com>
2016-09-15 03:43:15 +0200

    Fix __repr__() using last_print_t instead of start_t #269
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/_tqdm.py
+++ tqdm/_tqdm.py
@@ -651,7 +651,7 @@ class tqdm(object):
 
     def __repr__(self):
         return self.format_meter(self.n, self.total,
-                                 self._time() - self.last_print_t,
+                                 self._time() - self.start_t,
                                  self.ncols, self.desc, self.ascii, self.unit,
                                  self.unit_scale, 1 / self.avg_time
                                  if self.avg_time else None, self.bar_format)
