fbb4071f298ba7791168b0078e1a8a6a193c6476
Stephen L <lrq3000@gmail.com>
2016-10-17 03:38:30 +0200

    Fix tqdm_bare_class import
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -2,15 +2,15 @@ from ._tqdm import tqdm
 from ._tqdm import trange
 from ._tqdm_gui import tqdm_gui
 from ._tqdm_gui import tgrange
-from ._tqdm_bare import tqdm_bare
-from ._tqdm_bare import tbrange
+from ._tqdm_bare import tqdm_bare_class
+from ._tqdm_bare import tbcrange
 from ._tqdm_pandas import tqdm_pandas
 from ._main import main
 from ._version import __version__  # NOQA
 from ._tqdm import TqdmTypeError, TqdmKeyError, TqdmDeprecationWarning
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange',
-           'tqdm_bare', 'tqdm_brange',
+           'tqdm_bare_class', 'tbcrange',
            'tqdm_pandas',
            'tqdm_notebook', 'tnrange', 'main',
            'TqdmTypeError', 'TqdmKeyError', 'TqdmDeprecationWarning',
