fae534fc79ee4a70153da2afa40b273b93632e5e
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2016-10-31 00:26:10 +0000

    fractional overhead

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,13 +1,20 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
+from __future__ import division
 
 
-class TimeSuite:
+class FractionalOverheadSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
     def setup(self):
+        try:
+            from time import process_time
+            self.time = process_time
+        except ImportError:
+            from time import clock
+            self.time = clock
         from tqdm import tqdm
         self.tqdm = tqdm
         try:
@@ -15,11 +22,14 @@ class TimeSuite:
         except:
             self.iterable = range(int(6e6))
 
-    def time_tqdm(self):
+        t0 = self.time()
+        [0 for _ in self.iterable]
+        t1 = self.time()
+        self.t = t1 - t0
+
+
+    def track_tqdm(self):
+        t0 = self.time()
         [0 for _ in self.tqdm(self.iterable)]
-
-
-class MemSuite:
-    # def mem_list(self):
-    #     return [0] * 256
-    pass
+        t1 = self.time()
+        return (t1 - t0 - self.t) / self.t  # fractional overhead
