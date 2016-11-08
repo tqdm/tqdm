7a1a91747c9ceab865855bbb0ca364184c32cfa9
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2016-11-08 22:02:17 +0000

    pty flush, (-s, --silent)

--- examples/7zx.py
+++ examples/7zx.py
@@ -6,6 +6,7 @@ Options:
   -h, --help     Print this help and exit
   -v, --version  Print version and exit
   -c, --compressed       Use compressed (instead of uncompressed) file sizes
+  -s, --silent   Do not print one row per zip file
   -y, --yes      Assume yes to all queries (for extraction)
   -D=<level>, --debug=<level>
                  Print various types of debugging information. Choices:
@@ -23,10 +24,12 @@ import logging as log
 import subprocess
 import re
 from tqdm import tqdm
-from distutils.spawn import find_executable
+import pty
+import os
+import io
 __author__ = "Casper da Costa-Luis <casper.dcl@physics.org>"
 __licence__ = "MPLv2.0"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __license__ = __licence__
 
 
@@ -56,43 +59,53 @@ def main():
         fcomp = dict((n, int(c if args['--compressed'] else u))
                      for (u, c, n) in finfo[:-1])
         # log.debug(fcomp)
+        # zips  : {'zipname' : {'filename' : int(size)}}
         zips[fn] = fcomp
 
     # Extract
     cmd7zx = ["7z", "x", "-bd"]
     if args['--yes']:
         cmd7zx += ["-y"]
-    if find_executable("stdbuf"):
-        # Force 7z to flush per-line (finer progress)
-        cmd7zx = ["stdbuf", "-oL"] + cmd7zx
     log.info("Extracting from {:d} file(s)".format(len(zips)))
-    with tqdm(total=sum(s for fcomp in zips.values()
-                        for (_, s) in fcomp.items()),
-              unit="B", unit_scale=True) as t:
+    with tqdm(total=sum(sum(fcomp.values()) for fcomp in zips.values()),
+              unit="B", unit_scale=True) as tall:
         for fn, fcomp in zips.items():
+            md, sd = pty.openpty()
             ex = subprocess.Popen(cmd7zx + [fn],
                                   bufsize=1,
-                                  stdout=subprocess.PIPE,
+                                  stdout=md,  # subprocess.PIPE,
                                   stderr=subprocess.STDOUT)
-            while True:
-                l_raw = ex.stdout.readline()
-                if not l_raw:
-                    break
-                l = l_raw.strip()
-                if l.startswith("Extracting"):
-                    exname = l.lstrip("Extracting").lstrip()
-                    t.update(fcomp.get(exname, 0))
-                # elif l:
-                #     if not any(l.startswith(i) for i in
-                #                ("7-Zip ",
-                #                 "p7zip Version ",
-                #                 "Processing archive: ",
-                #                 "Everything is Ok",
-                #                 "Folders: ",
-                #                 "Files: ",
-                #                 "Size: ",
-                #                 "Compressed: ")):
-                #         t.write(l)
+            os.close(sd)
+            with io.open(md, mode="rU", buffering=1) as m:
+                with tqdm(total=sum(fcomp.values()), disable=len(zips) < 2,
+                          leave=False, unit="B", unit_scale=True) as t:
+                    while True:
+                        try:
+                            l_raw = m.readline()
+                        except:
+                            break
+                        l = l_raw.strip()
+                        if l.startswith("Extracting"):
+                            exname = l.lstrip("Extracting").lstrip()
+                            s = fcomp.get(exname, 0)  # 0 is likely folders
+                            t.update(s)
+                            tall.update(s)
+                        elif l:
+                            if not any(l.startswith(i) for i in
+                                       ("7-Zip ",
+                                        "p7zip Version ",
+                                        "Everything is Ok",
+                                        "Folders: ",
+                                        "Files: ",
+                                        "Size: ",
+                                        "Compressed: ")):
+                                if l.startswith("Processing archive: "):
+                                    if not args['--silent']:
+                                        t.write(t.format_interval(
+                                            t.start_t - tall.start_t) + ' ' +
+                                            l.lstrip("Processing archive: "))
+                                else:
+                                    t.write(l)
             ex.wait()
 main.__doc__ = __doc__
 
