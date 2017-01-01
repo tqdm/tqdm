064aa65ce764e0b02e3543c4c7a8c53683c86feb
Stephen L <lrq3000@gmail.com>
2017-01-01 15:00:10 +0100

    check if in tqdm project before extra version calculation + style fixes

--- tqdm/_version.py
+++ tqdm/_version.py
@@ -11,35 +11,41 @@ __version__ = '.'.join(map(str, version_info))
 
 
 # auto -extra based on commit hash (if not tagged as release)
-res = None
 scriptdir = os.path.dirname(__file__)
 gitdir = os.path.abspath(scriptdir + '/../.git')
 if os.path.isdir(gitdir):
-    # Open the HEAD file
-    with open(os.path.join(gitdir,'HEAD'), 'r') as fh_head:
-        res = fh_head.readline().strip()
-    # If we are in a branch, HEAD will point to a file containing the latest commit
-    if 'ref:' in res:
-        # Get reference file path
-        ref_file = res[5:]
-        # Get branch name
-        branch_name = ref_file.split('/')[-1]
-        # Sanitize path of ref file
-        # get full path to ref file
-        ref_file_path = os.path.abspath(os.path.join(gitdir, ref_file))
-        # check that we are in git folder (by stripping the git folder from the ref file path)
-        if os.path.relpath(ref_file_path, gitdir).replace('\\', '/') != ref_file:
-            # Trying to get out of git folder, not good!
-            res = None
-        else:
-            # Else the file file is inside git, all good
-            # Open the ref file
-            with open(ref_file_path, 'r') as fh_branch:
-                commit_hash = fh_branch.readline().strip()
-                res = commit_hash[:8] + ' (branch: ' + branch_name + ')'
-    # Else we are in detached HEAD mode, we directly have a commit hash
-    else:
-        res = res[:8]
+    extra = None
+    # Open config file to check if we are in tqdm project
+    with open(os.path.join(gitdir, 'config'), 'r') as fh_config:
+        if 'tqdm' in fh_config.read():
+            # Open the HEAD file
+            with open(os.path.join(gitdir, 'HEAD'), 'r') as fh_head:
+                extra = fh_head.readline().strip()
+            # If we are in a branch, HEAD will point to a file containing the latest commit
+            if 'ref:' in extra:
+                # Get reference file path
+                ref_file = extra[5:]
+                # Get branch name
+                branch_name = ref_file.split('/')[-1]
+
+                # Sanitize path of ref file
+                # get full path to ref file
+                ref_file_path = os.path.abspath(os.path.join(gitdir, ref_file))
+                # check that we are in git folder (by stripping the git folder from the ref file path)
+                if os.path.relpath(ref_file_path, gitdir).replace('\\', '/') != ref_file:
+                    # Trying to get out of git folder, not good!
+                    extra = None
+                else:
+                    # Else the file file is inside git, all good
+                    # Open the ref file
+                    with open(ref_file_path, 'r') as fh_branch:
+                        commit_hash = fh_branch.readline().strip()
+                        extra = commit_hash[:8] + ' (branch: ' + branch_name + ')'
+
+            # Else we are in detached HEAD mode, we directly have a commit hash
+            else:
+                extra = extra[:8]  # limit to the first 8 symbols of the hash
+
     # Append to version string
-    if res is not None:
-        __version__ += '-' + res
+    if extra is not None:
+        __version__ += '-' + extra
--- tqdm/tests/tests_version.py
+++ tqdm/tests/tests_version.py
@@ -4,10 +4,10 @@ import re
 def test_version():
     """ Test version string """
     from tqdm import __version__
-    Mmpe = __version__.split()[0]  # remove part after space (branch name)
-    Mmpe = re.split('[.-]', Mmpe)  # split by dot and dash
-    assert 3 <= len(Mmpe) <= 4
+    version_parts = __version__.split()[0]  # remove part after space (branch name)
+    version_parts = re.split('[.-]', version_parts)  # split by dot and dash
+    assert 3 <= len(version_parts) <= 4
     try:
-        map(int, Mmpe[:3])
+        map(int, version_parts[:3])
     except:
         raise TypeError('Version major, minor, patch must be integers')
