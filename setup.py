795732207eb375800cee5a85ccd41aee2fbcb64c
Casper da Costa-Luis <casper.dcl@physics.org>
2017-01-16 09:48:29 +0000

    add Py3.6

--- setup.py
+++ setup.py
@@ -199,6 +199,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: IronPython',
         'Topic :: Software Development :: Libraries',
