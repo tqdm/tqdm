a432c3dacdaf166109bf6ba4d539b760c80534ed
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2018-04-15 01:16:33 +0100

    remove warning, flake8

--- tqdm/tests/tests_pandas.py
+++ tqdm/tests/tests_pandas.py
@@ -1,8 +1,7 @@
-from nose.plugins.skip import SkipTest
-import warnings
-
 from tqdm import tqdm
-from tests_tqdm import with_setup, pretest, posttest, StringIO, closing
+from tests_tqdm import with_setup, pretest, posttest, SkipTest, \
+    StringIO, closing
+
 
 @with_setup(pretest, posttest)
 def test_pandas_setup():
@@ -12,13 +11,6 @@ def test_pandas_setup():
         import pandas as pd
     except ImportError:
         raise SkipTest
-
-    with warnings.catch_warnings(record=True) as res:
-        warnings.simplefilter("always")
-        # set total in tqdm.pandas shall cause a warning
-        tqdm.pandas(total=100)
-        assert len(res) == 1
-        assert all([i in str(res[-1].message) for i in ['total','not recommended']])
 
     with closing(StringIO()) as our_file:
         tqdm.pandas(file=our_file, leave=True, ascii=True, total=123)
