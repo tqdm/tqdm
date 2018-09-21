15fb066c95f79cf4b834928e11351cdb87686fe1
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2018-09-21 17:23:14 +0100

    suppress TqdmExperimentalWarning in

--- tqdm/auto/__init__.py
+++ tqdm/auto/__init__.py
@@ -1,1 +1,6 @@
-from ..autonotebook import tqdm, trange
+import warnings
+from .._tqdm import TqdmExperimentalWarning
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=TqdmExperimentalWarning)
+    from ..autonotebook import tqdm, trange
+__all__ = ["tqdm", "trange"]
