eb9377f2999eb5e870bc60543395d2c566e1174b
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-01-19 16:35:24 +0000

    add itertools stub
    - addresses #225

---
+++ tqdm/contrib/itertools.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from tqdm.auto import tqdm as tqdm_auto
+from copy import deepcopy
+import itertools
+
+
+def product(*iterables, **tqdm_kwargs):
+    """
+    Equivalent of `itertools.product`.
+
+    Parameters
+    ----------
+    tqdm_class  : [default: tqdm.auto.tqdm].
+    """
+    kwargs = deepcopy(tqdm_kwargs)
+    tqdm_class = kwargs.pop("tqdm_class", tqdm_auto)
+    try:
+        lens = list(map(len, iterables))
+    except AttributeError:  # missing py2 len
+        total = None
+    except TypeError:  # missing py3 len
+        total = None
+    else:
+        total = 1
+        for i in lens:
+            total *= i
+        kwargs.setdefault("total", total)
+    with tqdm_class(**kwargs) as t:
+        for i in itertools.product(*iterables):
+            yield i
+            t.update()
