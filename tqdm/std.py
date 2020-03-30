4665954df53f51d489c97d3e853ede5c9f8636f9
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-03-30 01:33:09 +0100

    minor nested `clear()` bugfix

--- tqdm/std.py
+++ tqdm/std.py
@@ -1454,7 +1454,8 @@ class tqdm(Comparable):
         if pos >= nrows - 1:
             if pos >= nrows:
                 return
-            msg = " ... (more hidden) ..."
+            if msg:  # override at `nrows - 1`
+                msg = " ... (more hidden) ..."
 
         if pos:
             self.moveto(pos)
