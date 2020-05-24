8d721b243d380d344c4e0b20ed8b8d6b666fa782
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-05-24 12:58:58 +0100

    add contrib.bells
    All the bells & whistles!
    
    - related #614
    - realted #950

---
+++ tqdm/contrib/bells.py
@@ -0,0 +1,25 @@
+"""
+All the bells & whistles:
+
+- tqdm.auto
+- tqdm.tqdm.pandas
+- tqdm.contrib.telegram
+    + uses ${TQDM_TELEGRAM_TOKEN} and ${TQDM_TELEGRAM_CHAT_ID}
+- tqdm.contrib.discord
+    + uses ${TQDM_DISCORD_TOKEN} and ${TQDM_DISCORD_CHANNEL_ID}
+"""
+__all__ = ['tqdm', 'trange']
+from os import getenv
+import warnings
+
+
+if getenv("TQDM_TELEGRAM_TOKEN") and getenv("TQDM_TELEGRAM_CHAT_ID"):
+    from tqdm.contrib.telegram import tqdm, trange
+elif getenv("TQDM_DISCORD_TOKEN") and getenv("TQDM_DISCORD_CHANNEL_ID"):
+    from tqdm.contrib.discord import tqdm, trange
+else:
+    from tqdm.auto import tqdm, trange
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=FutureWarning)
+    tqdm.pandas()
