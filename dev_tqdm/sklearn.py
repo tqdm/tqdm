cb20857d80d4509611072baaea9cfaa34c22ae24
Andres Torres <34612646+TurretAA12@users.noreply.github.com>
2020-06-12 17:45:33 -0700

    Added tqdm integration with the 'permutation_test_score' function

--- dev_tqdm/sklearn.py
+++ dev_tqdm/sklearn.py
@@ -1,5 +1,6 @@
 @classmethod
 def sklearn(tclass, *targs, **tkwargs):
+    import collections
     import functools
     import warnings
     from sklearn import model_selection
@@ -7,21 +8,29 @@ def sklearn(tclass, *targs, **tkwargs):
     def progress_cross_val(option, estimator, X, *args, cv=5, **kwargs):
         if 'verbose' in kwargs and kwargs['verbose'] >= 1:
             warnings.warn('Using verbose with tqdm can cause display issues with tqdm and/or the verbose messages', category=SyntaxWarning)
-        valid_options = ['predict', 'score', 'validate', 'learning_curve']
+        valid_options = ['predict', 'score', 'validate', 'learning_curve', 'permutation_test_score']
         assert option in valid_options, f"[tqdm: Internal] progress_cross_val() {option} not in valid options"
+
         option, validate = ('score', True) if option == 'validate' else (option, False)
         option, learning_curve = ('score', True) if option == 'learning_curve' else (option, False)
+        option, permutation_test_score = ('score', True) if option == 'permutation_test_score' else (option, False)
 
         if hasattr(cv, 'n_splits'):
             parsed_cv = cv.n_splits
-        elif isinstance(cv, Iterable):
+        elif isinstance(cv, collections.Iterable):
             parsed_cv = len(list(cv))
+        else:
+            parsed_cv = cv
 
         if 'total' in kwargs:
             # Maybe remove this? IDK
             total = twargs['total']
         elif learning_curve:
-            total = 2 * len(kwargs['train_sizes']) * parsed_cv if 'train_sizes' in kwargs else parsed_cv * 5 * 2 # The extra two is required because the `learning_curve` function trains on both training set and testing sets
+            total = len(kwargs['train_sizes']) if 'train_sizes' in kwargs else 5
+            total *= parsed_cv * 2 # The extra two is required because the `learning_curve` function trains on both training set and testing sets
+        elif permutation_test_score:
+            total = kwargs['n_permutations'] if 'n_permutations' in kwargs else 100
+            total = total * parsed_cv + parsed_cv
         else:
             total = parsed_cv
 
@@ -35,7 +44,15 @@ def sklearn(tclass, *targs, **tkwargs):
                     t.update()
                     return func(self, *margs, **mkwargs)
                 setattr(estimator.__class__, option, functools.partialmethod(update, _save_me))
-                return getattr(model_selection, 'learning_curve' if learning_curve else f"cross_{'validate' if validate else f'val_{option}'}")(estimator, X, *args, cv=cv, **kwargs)
+
+                if learning_curve:
+                    final_func = 'learning_curve'
+                elif permutation_test_score:
+                    final_func = 'permutation_test_score'
+                else:
+                    final_func = f"cross_{'validate' if validate else f'val_{option}'}"
+
+                return getattr(model_selection, final_func)(estimator, X, *args, cv=cv, **kwargs)
         finally:
             setattr(estimator.__class__, option, _save_me)
 
@@ -44,7 +61,9 @@ def sklearn(tclass, *targs, **tkwargs):
     cross_val_score_alias = ['progress_cross_val_score', 'pcvs', 'pcross_val_score']
     cross_validate_alias = ['progress_cross_validate', 'pcv', 'pcross_validate']
     learning_curve_alias = ['progress_learning_curve', 'plc', 'plearning_curve']
-    for name in cross_val_predict_alias + cross_val_score_alias + cross_validate_alias + learning_curve_alias:
+    permutation_test_score_alias = ['progress_permutation_test_score', 'ppts', 'ppermutation_test_score']
+    aliases = cross_val_predict_alias + cross_val_score_alias + cross_validate_alias + learning_curve_alias + permutation_test_score_alias
+    for name in aliases:
         if name in cross_val_predict_alias:
             option = 'predict'
         elif name in cross_val_score_alias:
@@ -53,4 +72,6 @@ def sklearn(tclass, *targs, **tkwargs):
             option = 'validate'
         elif name in learning_curve_alias:
             option = 'learning_curve'
+        elif name in permutation_test_score_alias:
+            option = 'permutation_test_score'
         setattr(model_selection, name, functools.partial(progress_cross_val, option))
