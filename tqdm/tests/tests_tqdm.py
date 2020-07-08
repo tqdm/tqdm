90078a5e811b6b5f512c1c827b909f4469bcbc56
HeoHeo <h0h6h2h5@gmail.com>
2020-07-08 18:51:45 +0900

    Fix miniters to work
    Default mininterval(0.1) makes miniters to not work.
    
    Co-authored-by: pica4500 <pica4500@gmail.com>
    Co-authored-by: KOOOOO <goo1514@naver.com>
    Co-authored-by: namwoong-kim <kimnamwoong12@gmail.com>

--- tqdm/tests/tests_tqdm.py
+++ tqdm/tests/tests_tqdm.py
@@ -676,12 +676,12 @@ def test_max_interval():
 def test_min_iters():
     """Test miniters"""
     with closing(StringIO()) as our_file:
-        for _ in tqdm(_range(3), file=our_file, leave=True, miniters=4):
+        for _ in tqdm(_range(3), file=our_file, leave=True, mininterval=0, miniters=4):
             our_file.write('blank\n')
         assert '\nblank\nblank\n' in our_file.getvalue()
 
     with closing(StringIO()) as our_file:
-        for _ in tqdm(_range(3), file=our_file, leave=True, miniters=1):
+        for _ in tqdm(_range(3), file=our_file, leave=True, mininterval=0, miniters=1):
             our_file.write('blank\n')
         # assume automatic mininterval = 0 means intermediate output
         assert '| 3/3 ' in our_file.getvalue()
