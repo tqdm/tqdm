947e6a2130cc385ed365d939e37ddf02ce2a2b7c
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-10-24 23:26:18 +0100

    fix py3 CLI bug, fix py3 tests

--- tests/tests_main.py
+++ tests/tests_main.py
@@ -8,7 +8,7 @@ import subprocess
 from tqdm.cli import main, TqdmKeyError, TqdmTypeError
 from tqdm.utils import IS_WIN
 from .tests_tqdm import pretest_posttest  # NOQA
-from .tests_tqdm import skip, _range, closing, UnicodeIO, StringIO
+from .tests_tqdm import skip, _range, closing, UnicodeIO, StringIO, BytesIO
 
 
 def _sh(*cmd, **kwargs):
@@ -45,18 +45,19 @@ def test_pipes():
 def test_main():
     """Test misc CLI options"""
     _SYS = sys.stdin, sys.argv
+    _STDOUT = sys.stdout
     N = 123
 
     # test direct import
-    sys.stdin = [str(i).encode('utf-8') for i in _range(N)]
+    sys.stdin = [str(i).encode() for i in _range(N)]
     sys.argv = ['', '--desc', 'Test CLI import',
                 '--ascii', 'True', '--unit_scale', 'True']
     import tqdm.__main__  # NOQA
     sys.stderr.write("Test misc CLI options ... ")
 
     # test --delim
-    IN_DATA = '\0'.join(map(str, _range(N)))
-    with closing(StringIO()) as sys.stdin:
+    IN_DATA = '\0'.join(map(str, _range(N))).encode()
+    with closing(BytesIO()) as sys.stdin:
         sys.argv = ['', '--desc', 'Test CLI delim',
                     '--ascii', 'True', '--delim', r'\0', '--buf_size', '64']
         sys.stdin.write(IN_DATA)
@@ -67,8 +68,8 @@ def test_main():
             assert str(N) + "it" in fp.getvalue()
 
     # test --bytes
-    IN_DATA = IN_DATA.replace('\0', '\n')
-    with closing(StringIO()) as sys.stdin:
+    IN_DATA = IN_DATA.replace(b'\0', b'\n')
+    with closing(BytesIO()) as sys.stdin:
         sys.stdin.write(IN_DATA)
         sys.stdin.seek(0)
         sys.argv = ['', '--ascii', '--bytes=True', '--unit_scale', 'False']
@@ -77,30 +78,32 @@ def test_main():
             assert str(len(IN_DATA)) in fp.getvalue()
 
     # test --log
-    sys.stdin = map(str, _range(N))
+    sys.stdin = [str(i).encode() for i in _range(N)]
     # with closing(UnicodeIO()) as fp:
     main(argv=['--log', 'DEBUG'], fp=NULL)
     # assert "DEBUG:" in sys.stdout.getvalue()
 
-    # test --tee
-    with closing(StringIO()) as sys.stdin:
-        sys.stdin.write(IN_DATA)
-
-        sys.stdin.seek(0)
-        with closing(UnicodeIO()) as fp:
-            main(argv=['--mininterval', '0', '--miniters', '1'], fp=fp)
-            res = len(fp.getvalue())
-            # assert len(fp.getvalue()) < len(sys.stdout.getvalue())
-
-        sys.stdin.seek(0)
-        with closing(UnicodeIO()) as fp:
-            main(argv=['--tee', '--mininterval', '0', '--miniters', '1'], fp=fp)
-            # spaces to clear intermediate lines could increase length
-            assert len(fp.getvalue()) >= res + len(IN_DATA)
-
-    # test --null
-    _STDOUT = sys.stdout
     try:
+        # test --tee
+        IN_DATA = IN_DATA.decode()
+        with closing(StringIO()) as sys.stdout:
+            with closing(StringIO()) as sys.stdin:
+                sys.stdin.write(IN_DATA)
+
+                sys.stdin.seek(0)
+                with closing(UnicodeIO()) as fp:
+                    main(argv=['--mininterval', '0', '--miniters', '1'], fp=fp)
+                    res = len(fp.getvalue())
+                    # assert len(fp.getvalue()) < len(sys.stdout.getvalue())
+
+                sys.stdin.seek(0)
+                with closing(UnicodeIO()) as fp:
+                    main(argv=['--tee', '--mininterval', '0',
+                               '--miniters', '1'], fp=fp)
+                    # spaces to clear intermediate lines could increase length
+                    assert len(fp.getvalue()) >= res + len(IN_DATA)
+
+        # test --null
         with closing(StringIO()) as sys.stdout:
             with closing(StringIO()) as sys.stdin:
                 sys.stdin.write(IN_DATA)
@@ -117,14 +120,12 @@ def test_main():
                 with closing(UnicodeIO()) as fp:
                     main(argv=[], fp=fp)
                     assert sys.stdout.getvalue()
-    except:
-        sys.stdout = _STDOUT
-        raise
-    else:
+    finally:
         sys.stdout = _STDOUT
 
     # test integer --update
-    with closing(StringIO()) as sys.stdin:
+    IN_DATA = IN_DATA.encode()
+    with closing(BytesIO()) as sys.stdin:
         sys.stdin.write(IN_DATA)
 
         sys.stdin.seek(0)
@@ -134,8 +135,8 @@ def test_main():
             assert str(N // 2 * N) + "it" in res  # arithmetic sum formula
 
     # test integer --update --delim
-    with closing(StringIO()) as sys.stdin:
-        sys.stdin.write(IN_DATA.replace('\n', 'D'))
+    with closing(BytesIO()) as sys.stdin:
+        sys.stdin.write(IN_DATA.replace(b'\n', b'D'))
 
         sys.stdin.seek(0)
         with closing(UnicodeIO()) as fp:
@@ -144,7 +145,7 @@ def test_main():
             assert str(N // 2 * N) + "it" in res  # arithmetic sum formula
 
     # test integer --update_to
-    with closing(StringIO()) as sys.stdin:
+    with closing(BytesIO()) as sys.stdin:
         sys.stdin.write(IN_DATA)
 
         sys.stdin.seek(0)
@@ -155,8 +156,8 @@ def test_main():
             assert str(N) + "it" not in res
 
     # test integer --update_to --delim
-    with closing(StringIO()) as sys.stdin:
-        sys.stdin.write(IN_DATA.replace('\n', 'D'))
+    with closing(BytesIO()) as sys.stdin:
+        sys.stdin.write(IN_DATA.replace(b'\n', b'D'))
 
         sys.stdin.seek(0)
         with closing(UnicodeIO()) as fp:
@@ -166,8 +167,8 @@ def test_main():
             assert str(N) + "it" not in res
 
     # test float --update_to
-    IN_DATA = '\n'.join((str(i / 2.0) for i in _range(N)))
-    with closing(StringIO()) as sys.stdin:
+    IN_DATA = '\n'.join((str(i / 2.0) for i in _range(N))).encode()
+    with closing(BytesIO()) as sys.stdin:
         sys.stdin.write(IN_DATA)
 
         sys.stdin.seek(0)
