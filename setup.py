6b4a6b5372e72b2ba6b7b3fc9890c56a943d5586
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2020-11-16 00:49:47 +0000

    test py3.9
    - closes #1073

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: IronPython',
         'Programming Language :: Python :: Implementation :: PyPy',
