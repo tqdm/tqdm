12614aa54ca830ff7dd29e72b00f2e007dced933
Casper da Costa-Luis <tqdm@caspersci.uk.to>
2021-02-17 04:15:29 +0000

    minor framework tidy

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import os
 import sys
+from os import path
 
 from setuptools import setup
 
-src_dir = os.path.abspath(os.path.dirname(__file__))
+src_dir = path.abspath(path.dirname(__file__))
 if sys.argv[1].lower().strip() == 'make':  # exec Makefile commands
     import pymake
-    fpath = os.path.join(src_dir, 'Makefile')
+    fpath = path.join(src_dir, 'Makefile')
     pymake.main(['-f', fpath] + sys.argv[2:])
     # Stop to avoid setup.py raising non-standard command error
     sys.exit(0)
