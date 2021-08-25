03a8b6cb498f3ec5c05063f815f57bf63bb89eb1
zsecducna <76715938+zsecducna@users.noreply.github.com>
2021-08-25 15:19:42 +0700

    Don't print new line for status bar

--- tqdm/std.py
+++ tqdm/std.py
@@ -614,7 +614,7 @@ class tqdm(Comparable):
                     inst.pos = abs(instance.pos)
 
     @classmethod
-    def write(cls, s, file=None, end="\n", nolock=False):
+    def write(cls, s, file=None, end="\r", nolock=False):
         """Print a message via tqdm (without overlap with bars)."""
         fp = file if file is not None else sys.stdout
         with cls.external_write_mode(file=file, nolock=nolock):
