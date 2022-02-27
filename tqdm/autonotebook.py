7cdbfee224f94c8bc63dceaab42637962f2f2a04
Casper da Costa-Luis <tqdm@cdcl.ml>
2022-02-27 21:19:56 +0000

    autonotebook: check for ipywidgets
    - closes #1218
    - fixes #1082
    - fixes #1217

--- tqdm/autonotebook.py
+++ tqdm/autonotebook.py
@@ -8,6 +8,7 @@ Usage:
 """
 import os
 import sys
+from warnings import warn
 
 try:
     get_ipython = sys.modules['IPython'].get_ipython
@@ -15,11 +16,14 @@ try:
         raise ImportError("console")
     if 'VSCODE_PID' in os.environ:  # pragma: no cover
         raise ImportError("vscode")
+    from .notebook import WARN_NOIPYW, IProgress
+    if IProgress is None:
+        from .std import TqdmWarning
+        warn(WARN_NOIPYW, TqdmWarning, stacklevel=2)
+        raise ImportError('ipywidgets')
 except Exception:
     from .std import tqdm, trange
 else:  # pragma: no cover
-    from warnings import warn
-
     from .notebook import tqdm, trange
     from .std import TqdmExperimentalWarning
     warn("Using `tqdm.autonotebook.tqdm` in notebook mode."
