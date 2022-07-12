70d6e5692cd9068e4e8b9a8de1fb9032dfff6556
Sun Yijiang <sunyijiang@gmail.com>
2022-07-12 11:42:38 +0800

    Update std.py
    Default disable to None.

--- tqdm/std.py
+++ tqdm/std.py
@@ -845,7 +845,7 @@ class tqdm(Comparable):
 
     def __init__(self, iterable=None, desc=None, total=None, leave=True, file=None,
                  ncols=None, mininterval=0.1, maxinterval=10.0, miniters=None,
-                 ascii=None, disable=False, unit='it', unit_scale=False,
+                 ascii=None, disable=None, unit='it', unit_scale=False,
                  dynamic_ncols=False, smoothing=0.3, bar_format=None, initial=0,
                  position=None, postfix=None, unit_divisor=1000, write_bytes=None,
                  lock_args=None, nrows=None, colour=None, delay=0, gui=False,
