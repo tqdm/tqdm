a7a99b9d5cc0c950e86913175237c18f06640d7c
Manfrid Strengell <103531280+yaanae@users.noreply.github.com>
2022-08-05 12:27:47 +0200

    Added back asyncio
    For some reason the asyncio part of the package had no imports, so I added that back.

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -7,6 +7,7 @@ from .std import (
     TqdmDeprecationWarning, TqdmExperimentalWarning, TqdmKeyError, TqdmMonitorWarning,
     TqdmTypeError, TqdmWarning, tqdm, trange)
 from .version import __version__
+from . import asyncio
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
            'tqdm_notebook', 'tnrange', 'main', 'TMonitor',
