83fbb59e943d26e8fca0aa1408c0bed525b5dc43
Manfrid Strengell <103531280+yaanae@users.noreply.github.com>
2022-08-05 15:49:30 +0200

    Fixed style and tests

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -1,3 +1,4 @@
+from . import asyncio  # noqa
 from ._monitor import TMonitor, TqdmSynchronisationWarning
 from ._tqdm_pandas import tqdm_pandas
 from .cli import main  # TODO: remove in v5.0.0
@@ -7,7 +8,6 @@ from .std import (
     TqdmDeprecationWarning, TqdmExperimentalWarning, TqdmKeyError, TqdmMonitorWarning,
     TqdmTypeError, TqdmWarning, tqdm, trange)
 from .version import __version__
-from . import asyncio
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
            'tqdm_notebook', 'tnrange', 'main', 'TMonitor',
