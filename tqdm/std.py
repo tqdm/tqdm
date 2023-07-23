6c05732673a713501f993d3d7e212fc461703548
Harmen Wassenaar <harmen.wassenaar@target-holding.nl>
2023-07-23 19:54:06 +0200

    ensure __iter__ uses the iterator set at time of calling

--- tqdm/std.py
+++ tqdm/std.py
@@ -1160,37 +1160,44 @@ class tqdm(Comparable):
         # Inlining instance variables as locals (speed optimisation)
         iterable = self.iterable
 
-        # If the bar is disabled, then just walk the iterable
-        # (note: keep this check outside the loop for performance)
-        if self.disable:
-            for obj in iterable:
-                yield obj
-            return
+        # Wrap yielding code, so `__iter__` itself isn't a generator and `iterable` is set at the time
+        # `__iter__` is called, rather than when the first element is required. This is needed because
+        # `__reversed__` swaps the iterator with a reversed one, stores the generator returned by
+        # `__iter__()`, then swaps the old iterator back.
+        def generator():
+            # If the bar is disabled, then just walk the iterable
+            # (note: keep this check outside the loop for performance)
+            if self.disable:
+                for obj in iterable:
+                    yield obj
+                return
 
-        mininterval = self.mininterval
-        last_print_t = self.last_print_t
-        last_print_n = self.last_print_n
-        min_start_t = self.start_t + self.delay
-        n = self.n
-        time = self._time
+            mininterval = self.mininterval
+            last_print_t = self.last_print_t
+            last_print_n = self.last_print_n
+            min_start_t = self.start_t + self.delay
+            n = self.n
+            time = self._time
 
-        try:
-            for obj in iterable:
-                yield obj
-                # Update and possibly print the progressbar.
-                # Note: does not call self.update(1) for speed optimisation.
-                n += 1
+            try:
+                for obj in iterable:
+                    yield obj
+                    # Update and possibly print the progressbar.
+                    # Note: does not call self.update(1) for speed optimisation.
+                    n += 1
 
-                if n - last_print_n >= self.miniters:
-                    cur_t = time()
-                    dt = cur_t - last_print_t
-                    if dt >= mininterval and cur_t >= min_start_t:
-                        self.update(n - last_print_n)
-                        last_print_n = self.last_print_n
-                        last_print_t = self.last_print_t
-        finally:
-            self.n = n
-            self.close()
+                    if n - last_print_n >= self.miniters:
+                        cur_t = time()
+                        dt = cur_t - last_print_t
+                        if dt >= mininterval and cur_t >= min_start_t:
+                            self.update(n - last_print_n)
+                            last_print_n = self.last_print_n
+                            last_print_t = self.last_print_t
+            finally:
+                self.n = n
+                self.close()
+
+        return generator()
 
     def update(self, n=1):
         """
