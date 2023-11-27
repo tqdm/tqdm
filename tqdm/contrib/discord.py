ad28defb0c7dcae3a4ea6eaea36a5d50b49d82ad
guigoruiz1 <57478888+guigoruiz1@users.noreply.github.com>
2023-11-27 21:30:32 +0000

    Minor change

--- tqdm/contrib/discord.py
+++ tqdm/contrib/discord.py
@@ -30,7 +30,7 @@ class DiscordIO(MonoWorker):
             # Wait for the bot to be ready before continuing
             self.loop.run_until_complete(self.client.wait_until_ready())
             # Attempt to get the channel
-            channel = self.client.get_channel(channel_id)
+            channel = self.client.get_channel(int(channel_id))
             if channel:
                 # Ensure the bot has the necessary permissions to send messages
                 if channel.permissions_for(channel.guild.me).send_messages:
