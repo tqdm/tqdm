0e1de202b5057dd1ecf10f527e6eafa26af8a2a6
DinhHuy2010 <huy04052010@outlook.com>
2024-12-05 18:02:27 +0700

    fix __init__

--- tqdm/__init__.py
+++ tqdm/__init__.py
@@ -1,13 +1,22 @@
 """tqdm - fast, extensible progress bar for Python and CLI"""
 
+from typing import Any
+
 from ._monitor import TMonitor, TqdmSynchronisationWarning
 from ._tqdm_pandas import tqdm_pandas
 from .cli import main  # TODO: remove in v5.0.0
 from .gui import tqdm as tqdm_gui  # TODO: remove in v5.0.0
 from .gui import trange as tgrange  # TODO: remove in v5.0.0
 from .std import (
-    TqdmDeprecationWarning, TqdmExperimentalWarning, TqdmKeyError, TqdmMonitorWarning,
-    TqdmTypeError, TqdmWarning, tqdm, trange)
+    TqdmDeprecationWarning,
+    TqdmExperimentalWarning,
+    TqdmKeyError,
+    TqdmMonitorWarning,
+    TqdmTypeError,
+    TqdmWarning,
+    tqdm,
+    trange,
+)
 from .version import __version__
 
 __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
@@ -19,7 +28,7 @@ __all__ = ['tqdm', 'tqdm_gui', 'trange', 'tgrange', 'tqdm_pandas',
            '__version__']
 
 
-def tqdm_notebook(*args, **kwargs):  # pragma: no cover
+def tqdm_notebook(*args: Any, **kwargs: Any):  # pragma: no cover
     """See tqdm.notebook.tqdm for full documentation"""
     from warnings import warn
 
@@ -30,7 +39,7 @@ def tqdm_notebook(*args, **kwargs):  # pragma: no cover
     return _tqdm_notebook(*args, **kwargs)
 
 
-def tnrange(*args, **kwargs):  # pragma: no cover
+def tnrange(*args: Any, **kwargs: Any):  # pragma: no cover
     """Shortcut for `tqdm.notebook.tqdm(range(*args), **kwargs)`."""
     from warnings import warn
 
