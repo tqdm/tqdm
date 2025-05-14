a0671028667fc559cdf49bfa2866dc2bc05c8d1e
denis <justwantpost@gmail.com>
2025-05-14 13:21:35 +0200

    Added simpler solution added proper tests and example to demo notebook

--- tqdm/asyncio_notebook.py
+++ tqdm/asyncio_notebook.py
@@ -6,78 +6,19 @@ Usage:
 >>> async for i in trange(10):
 ...     ...
 """
-import asyncio
-from sys import version_info
+from .asyncio import tqdm_asyncio
+from .notebook import tqdm_notebook
 
-from .notebook import tqdm as std_tqdm  # notebook version
-
-__author__ = {"github.com/": ["casperdcl"]}
+__author__ = {"github.com/": ["grach0v"]}
 __all__ = ['tqdm_asyncio_notebook', 'tarange', 'tqdm', 'trange']
 
-class tqdm_asyncio_notebook(std_tqdm):
+
+class tqdm_asyncio_notebook(tqdm_asyncio, tqdm_notebook):
     """
     Asynchronous-friendly version of tqdm for Jupyter notebooks.
+    MRO behaviour as if `tqdm_asyncio` is inherited after `tqdm_notebook`, instead of `std_tqdm`.
     """
-    def __init__(self, iterable=None, *args, **kwargs):
-        super().__init__(iterable, *args, **kwargs)
-        self.iterable_awaitable = False
-        if iterable is not None:
-            if hasattr(iterable, "__anext__"):
-                self.iterable_next = iterable.__anext__
-                self.iterable_awaitable = True
-            elif hasattr(iterable, "__next__"):
-                self.iterable_next = iterable.__next__
-            else:
-                self.iterable_iterator = iter(iterable)
-                self.iterable_next = self.iterable_iterator.__next__
-
-    def __aiter__(self):
-        return self
-
-    async def __anext__(self):
-        try:
-            if self.iterable_awaitable:
-                res = await self.iterable_next()
-            else:
-                res = self.iterable_next()
-            self.update()
-            return res
-        except StopIteration:
-            self.close()
-            raise StopAsyncIteration
-        except BaseException:
-            self.close()
-            raise
-
-    def send(self, *args, **kwargs):
-        return self.iterable.send(*args, **kwargs)
-
-    @classmethod
-    def as_completed(cls, fs, *, loop=None, timeout=None, total=None, **tqdm_kwargs):
-        """
-        Wrapper for `asyncio.as_completed` with progress bar.
-        """
-        if total is None:
-            total = len(fs)
-        kwargs = {}
-        if version_info[:2] < (3, 10):
-            kwargs['loop'] = loop
-        # yield futures wrapped in progress bar
-        yield from cls(asyncio.as_completed(fs, timeout=timeout, **kwargs),
-                       total=total, **tqdm_kwargs)
-
-    @classmethod
-    async def gather(cls, *fs, loop=None, timeout=None, total=None, **tqdm_kwargs):
-        """
-        Wrapper for `asyncio.gather` with progress bar.
-        """
-        async def wrap_awaitable(i, f):
-            return i, await f
-
-        ifs = [wrap_awaitable(i, f) for i, f in enumerate(fs)]
-        res = [await f for f in cls.as_completed(ifs, loop=loop, timeout=timeout,
-                                                 total=total, **tqdm_kwargs)]
-        return [i for _, i in sorted(res)]
+    pass
 
 
 def tarange(*args, **kwargs):
@@ -87,6 +28,7 @@ def tarange(*args, **kwargs):
     return tqdm_asyncio_notebook(range(*args), **kwargs)
 
 # Aliases
+
 
 tqdm = tqdm_asyncio_notebook
 trange = tarange
