4b3395272d66a00d7d4eebec5a4e19ae679aa95d
Casper da Costa-Luis <tqdm@cdcl.ml>
2026-06-09 13:54:09 +0100

    revert accidental change to `tqdm(ascii)` default
    - partially reverts 17f246b

--- tqdm/std.py
+++ tqdm/std.py
@@ -952,7 +952,7 @@ class tqdm(Comparable):
                                             'position': int, 'nrows': int})
     def __init__(self, iterable=None, desc=None, total=None, leave=True, file=None,
                  ncols=None, mininterval=0.1, maxinterval=10.0, miniters=None,
-                 ascii=False,  # pylint: disable=redefined-builtin
+                 ascii=None,  # pylint: disable=redefined-builtin
                  disable=False, unit='it', unit_scale=False, dynamic_ncols=False, smoothing=0.3,
                  bar_format=None, initial=0, position=None, postfix=None, unit_divisor=1000,
                  write_bytes=False, lock_args=None, nrows=None, colour=None, delay=0.0, gui=False,
