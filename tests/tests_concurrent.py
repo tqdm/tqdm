604193aa03b5f5c0ad5f3d441951f0fbb515f1e4
Casper da Costa-Luis <tqdm@cdcl.ml>
2026-08-07 17:16:37 +0100

    tests: slight tidy

--- tests/tests_concurrent.py
+++ tests/tests_concurrent.py
@@ -7,7 +7,7 @@ from pytest import warns
 
 from tqdm.contrib.concurrent import interpreter_map, process_map, thread_map
 
-from .tests_tqdm import StringIO, TqdmWarning, closing, importorskip, mark, skip
+from .tests_tqdm import TqdmWarning, importorskip, mark, skip
 
 
 def incr(x):
@@ -15,41 +15,18 @@ def incr(x):
     return x + 1
 
 
-def test_thread_map():
-    """Test contrib.concurrent.thread_map"""
-    with closing(StringIO()) as our_file:
-        a = range(9)
-        b = [i + 1 for i in a]
-        try:
-            assert thread_map(lambda x: x + 1, a, file=our_file) == b
-        except ImportError as err:
-            skip(str(err))
-        assert thread_map(incr, a, file=our_file) == b
-
-
-@mark.skipif(sys.version_info < (3, 14), reason="requires Python 3.14+")
-def test_interpreter_map(capsys):
-    """Test contrib.concurrent.interpreter_map"""
+@mark.parametrize("mapper", [interpreter_map, process_map, thread_map])
+def test_concurrent_map(mapper, capsys):
+    """Test contrib.concurrent.*_map"""
     a = range(9)
     b = [i + 1 for i in a]
     try:
-        assert interpreter_map(incr, a) == b
+        assert mapper(incr, a, miniters=1) == b
     except ImportError as err:
         skip(str(err))
     out, err = capsys.readouterr()
     assert not out
-    assert '9/9' in err
-
-
-def test_process_map():
-    """Test contrib.concurrent.process_map"""
-    with closing(StringIO()) as our_file:
-        a = range(9)
-        b = [i + 1 for i in a]
-        try:
-            assert process_map(incr, a, file=our_file) == b
-        except ImportError as err:
-            skip(str(err))
+    assert '0/9' in err
 
 
 def check_lock(args):
